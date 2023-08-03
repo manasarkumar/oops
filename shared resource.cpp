77d89caa57f01ae1048f1eb23f81ebc1f4f25cda
manasarkumar <141334976+manasarkumar@users.noreply.github.com>
2023-08-03 19:07:38 +0530

    Create shared resource.cpp

---
+++ shared resource.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class SharedResource {
+private:
+ static int resource; // Shared resource
+public:
+ void accessResource() {
+ if (resource > 0) {
+ cout << "Access granted to the shared resource." << endl;
+ resource--;
+ } else {
+ cout << "Access denied. Shared resource is currently unavailable." << endl;
+ }
+ }
+ static void setResourceCount(int count) {
+ resource = count;
+ }
+};
+int SharedResource::resource = 0; // Initialize static variable
+int main() {
+ SharedResource obj1;
+ SharedResource obj2;
+ SharedResource::setResourceCount(5); // Set the initial count of the shared resource
+ obj1.accessResource(); // Access granted
+ obj2.accessResource(); // Access granted
+ obj1.accessResource(); // Access granted
+ obj2.accessResource(); // Access granted
+ obj1.accessResource(); // Access granted
+ obj2.accessResource(); // Access granted
+ obj1.accessResource(); // Access denied
+ obj2.accessResource(); // Access denied
+ return 0;
+}
